c7fc73f0458559382628e90595410bcc93b4b33e
Dani Riera <info@driera.com>
2021-11-22 16:49:24 +0100

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import legacy from "@vitejs/plugin-legacy";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import path from "path";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [legacy(), reactRefresh()],
+  esbuild: {
+    jsxInject: `import React from 'react'`, // automatically import React in jsx files
+  },
+  resolve: {
+    alias: [
+      {
+        // for import like : @/x/y/z
+        find: /@\//,
+        replacement: `${path.resolve(__dirname, "src")}/`,
+      },
+      {
+        // for import like : ~x/y/z
+        find: /~(.*)/,
+        replacement: `${path.resolve(__dirname, "node_modules")}/$1`,
+      },
+    ],
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        secure: false,
+        rewrite: path => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
