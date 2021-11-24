5da5e5fecbb63c9d2b4510db843ddc0361f97300
Dani Riera <daniel.riera@qatium.com>
2021-11-24 20:12:40 +0100

    Add tests to Button

--- src/components/Button.test.tsx
+++ src/components/Button.test.tsx
@@ -1,9 +1,32 @@
 import { render, screen } from "@testing-library/react";
-
 import Button from "./Button";
 
-it("should render", () => {
-  render(<Button />);
+describe("button", () => {
+  it("should render", () => {
+    render(<Button />);
 
-  expect(screen.getByRole("button", { name: /button/i })).toBeInTheDocument();
+    const defaultButton = screen.getByRole("button", { name: /button/i });
+    expect(defaultButton).toBeInTheDocument();
+  });
+
+  it("has an outline variant", () => {
+    render(<Button variant="outline" />);
+
+    const outlineButton = screen.getByRole("button", { name: /button/i });
+    expect(outlineButton).toHaveStyle({
+      color: "#0067F9",
+      border: "2px solid #0067F9"
+    });
+  });
+
+  it("has a text variant", () => {
+    render(<Button variant="text" />);
+
+    const textButton = screen.getByRole("button", { name: /button/i });
+    expect(textButton).toHaveStyle({
+      color: "#0067F9",
+      background: "transparent",
+      border: "none"
+    });
+  });
 });
