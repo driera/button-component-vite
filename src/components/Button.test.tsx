ec55a9bc94440932ef51de4ca0fbda04249dca2b
Dani Riera <daniel.riera@qatium.com>
2021-12-01 23:53:39 +0100

    Move to Stitches instead of styled-components

--- src/components/Button.test.tsx
+++ src/components/Button.test.tsx
@@ -9,36 +9,6 @@ describe("button", () => {
     expect(defaultButton).toBeInTheDocument();
   });
 
-  it("has an outline variant", () => {
-    render(<Button variant="outline" />);
-
-    const outlineButton = screen.getByRole("button", { name: /button/i });
-    expect(outlineButton).toHaveStyle({
-      color: "#0067F9",
-      border: "2px solid #0067F9"
-    });
-  });
-
-  it("has a text variant", () => {
-    render(<Button variant="text" />);
-
-    const textButton = screen.getByRole("button", { name: /button/i });
-    expect(textButton).toHaveStyle({
-      color: "#0067F9",
-      background: "transparent",
-      border: "none"
-    });
-  });
-
-  it("can disable the box shadow", () => {
-    render(<Button disableShadow />);
-
-    const defaultButton = screen.getByRole("button", { name: /button/i });
-    expect(defaultButton).toHaveStyle({
-      boxShadow: "none"
-    });
-  });
-
   it("can be disabled", () => {
     render(<Button disabled />);
 
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,56 +1,74 @@
-import styled from "styled-components";
+import { styled } from "@stitches/react";
 
 const primary_color = "#0067F9";
 const gray_color = {
-  medium: "#9E9E9E"
+  darkest: "#222222",
+  medium: "#9E9E9E",
+  light: "#AEAEAE",
+  lighter: "#E0E0E0"
 } as const;
 
-type BaseButtonProps = { disableShadow?: boolean };
-
-const BaseButton = styled.button(({ disableShadow }: BaseButtonProps) => ({
+const BaseButton = styled("button", {
   display: "block",
   padding: "16px 32px",
   border: "none",
   fontSize: "18px",
-  color: "#222222",
-  boxShadow: disableShadow ? "none" : "2px 2px 2px #00000022",
+  color: gray_color.darkest,
   borderRadius: "8px",
   transition: "background 0.15s ease-in",
   outline: "none",
 
-  "&&[disabled]": {
+  "&[disabled]": {
     color: gray_color.medium,
     cursor: "not-allowed"
   }
-}));
+});
 
-const DefaultButton = styled(BaseButton)({
-  background: "#E0E0E0",
+const DefaultButton = styled(BaseButton, {
+  boxShadow: "2px 2px 2px #00000022",
+  background: gray_color.lighter,
 
   "&:hover, &:focus": {
     "&:not([disabled])": {
-      background: "#AEAEAE"
+      background: gray_color.light
+    }
+  },
+
+  variants: {
+    disableShadow: {
+      true: {
+        boxShadow: "none"
+      }
     }
   }
 });
 
-const TextButton = styled(BaseButton)({
+const TextButton = styled(BaseButton, {
   color: primary_color,
   background: "transparent",
-  boxShadow: "none",
 
   "&:hover, &:focus": {
     "&:not([disabled])": {
       background: "#0067F933"
     }
+  },
+
+  variants: {
+    disabled: {
+      true: {
+        "&:hover, &:focus": {
+          background: "transparent"
+        }
+      }
+    }
   }
 });
 
-const OutlineButton = styled(TextButton)({
+const OutlineButton = styled(TextButton, {
   border: `2px solid ${primary_color}`,
 
-  "&[disabled]": {
-    border: `2px solid ${gray_color.medium}`
+  "&:disabled": {
+    border: "none"
   }
 });
 
@@ -67,14 +85,16 @@ const Button = ({
   disableShadow = false,
   disabled = false
 }: Props): JSX.Element => {
-  if (variant === "outline") {
-    return <OutlineButton disabled={disabled}>{children}</OutlineButton>;
-  }
   if (variant === "text") {
-    return <TextButton disabled={disabled}>{children}</TextButton>;
+    return <TextButton>{children}</TextButton>;
   }
+
+  if (variant === "outline") {
+    return <OutlineButton>{children}</OutlineButton>;
+  }
+
   return (
-    <DefaultButton disabled={disabled} disableShadow={disableShadow}>
+    <DefaultButton disableShadow={disableShadow} disabled={disabled}>
       {children}
     </DefaultButton>
   );
