105f36ca96270d82846db63c67c2efb1d75f3a78
Dani Riera <daniel.riera@qatium.com>
2021-12-04 18:59:22 +0100

    Update Readme & add a footer

--- README.md
+++ README.md
@@ -1,12 +1,64 @@
-# Button component
+<!-- Please update value in the {}  -->
 
-Component done following the next challenge at [devchallenge.io](https://devchallenges.io/challenges/ohgVTyJCbm5OZyTB2gNY)
+<h1 align="center">{Your project name}</h1>
 
-Scaffolding based on [fabien-ml/react-ts-vite-template](https://github.com/fabien-ml/react-ts-vite-template)
+<div align="center">
+   Solution for a challenge from  <a href="http://devchallenges.io" target="_blank">Devchallenges.io</a>.
+</div>
 
-## TO DO:
-## DONE:
-[√] Move to actions v2
+<div align="center">
+  <h3>
+    <a href="https://driera-button-component.netlify.app">
+      Demo
+    </a>
+    <span> | </span>
+    <a href="https://github.com/driera/button-component-vite/blob/main/src/components/Button.tsx">
+      Solution
+    </a>
+    <span> | </span>
+    <a href="https://devchallenges.io/challenges/ohgVTyJCbm5OZyTB2gNY">
+      Challenge
+    </a>
+  </h3>
+</div>
+
+<!-- TABLE OF CONTENTS -->
+
+## Table of Contents
+
+- [Overview](#overview)
+  - [Built With](#built-with)
+- [Features](#features)
+- [User stories](#user-stories)
+- [How to use](#how-to-use)
+- [Contact](#contact)
+
+<!-- OVERVIEW -->
+
+## Overview
+
+![screenshot](https://user-images.githubusercontent.com/693630/144719429-07e8a0d3-39c2-45e9-9759-29b465cbfe14.png)
+
+Creating a Button component is always a great exercise no matter how much experience you have. All things you have to consider to deliver an usable Button are nearly unlimited. In this exercise I just covered all features requested by user stories.
+
+### Built With
+
+<!-- This section should list any major frameworks that you built your project using. Here are a few examples.-->
+
+- [React](https://reactjs.org/)
+- [Stitches](https://stitches.dev/)
+- [React Testing Library](https://testing-library.com/docs/react-testing-library/intro/)
+- [Vite](https://vitejs.dev/)
+- [Typescript](https://www.typescriptlang.org/)
+- [Github actions](https://github.com/features/actions)
+- [Netlify](https://www.netlify.com/)
+## Features
+
+<!-- List the features of your application or follow the template. Don't share the figma file here :) -->
+
+This application/site was created as a submission to a [DevChallenges](https://devchallenges.io/challenges) challenge. The [challenge](https://devchallenges.io/challenges/ohgVTyJCbm5OZyTB2gNY) was to build an application to complete the given user stories.
+
+## User stories
   [√] User story: I can see different button types: default, outline and text
       [√] Default
       [√] Outline
@@ -19,3 +71,28 @@ Scaffolding based on [fabien-ml/react-ts-vite-template](https://github.com/fabie
   [√] User story: When I hover or focus, I can see visual indicators
   [√] User story (optional): Show button in a similar way like the design or use Storybook. Otherwise, showing the button in multiple states is enough
   [√] User story: I can still access all button attributes
+
+
+## How To Use
+
+<!-- This is an example, please update according to your application -->
+
+To clone and run this application, you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line:
+
+```bash
+# Clone this repository
+$ git clone https://github.com/your-user-name/your-project-name
+
+# Install dependencies
+$ npm install
+
+# Run the app
+$ npm run dev
+
+# Run tests
+$ npm run test-watch
+```
+
+## Contact
+
+- GitHub [@driera](https://github.com/driera)
--- src/App.tsx
+++ src/App.tsx
@@ -16,98 +16,125 @@ const Column = styled("div", {
   gap: 80
 });
 
+const Footer = styled("div", {
+  justifySelf: "stretch",
+  padding: "20px 0",
+  borderTop: "1px solid $gray_medium"
+});
+
 const Text = styled("p", {
   marginBottom: "1em",
   fontSize: "14px",
   color: "$gray_dark"
 });
 
+const SmallText = styled("p", {
+  fontSize: "12px",
+  color: "$gray_dark",
+  textAlign: "center"
+});
+
 const App = (): JSX.Element => {
   globalStyles();
 
   return (
-    <Container>
-      <h1>Button component</h1>
-      <Column>
+    <>
+      <Container>
+        <h1>Button component</h1>
+        <Column>
+          <div>
+            <Text>&lt;Button /&gt;</Text>
+            <Button color="primary">Button</Button>
+          </div>
+          <div>
+            <Text>{'<Button variant="outline" />'}</Text>
+            <Button variant="outline" color="primary">
+              Button
+            </Button>
+          </div>
+          <div>
+            <Text>{'<Button variant="text" />'}</Text>
+            <Button variant="text" color="primary">
+              Button
+            </Button>
+          </div>
+        </Column>
         <div>
-          <Text>&lt;Button /&gt;</Text>
-          <Button color="primary">Button</Button>
+          <Text>{"<Button disableShadow />"}</Text>
+          <Button disableShadow>Button</Button>
         </div>
         <div>
-          <Text>{'<Button variant="outline" />'}</Text>
-          <Button variant="outline" color="primary">
-            Button
-          </Button>
+          <Text>{"<Button disabled />"}</Text>
+          <Button disabled>Button</Button>
         </div>
-        <div>
-          <Text>{'<Button variant="text" />'}</Text>
-          <Button variant="text" color="primary">
-            Button
-          </Button>
-        </div>
-      </Column>
-      <div>
-        <Text>{"<Button disableShadow />"}</Text>
-        <Button disableShadow>Button</Button>
-      </div>
-      <div>
-        <Text>{"<Button disabled />"}</Text>
-        <Button disabled>Button</Button>
-      </div>
-      <Column>
-        <div>
-          <Text>{'<Button startIcon="anchor" />'}</Text>
-          <Button startIcon="anchor" color="primary">
-            Button
-          </Button>
-        </div>
-        <div>
-          <Text>{'<Button endIcon="rocket" />'}</Text>
-          <Button endIcon="rocket" color="danger">
-            Button
-          </Button>
-        </div>
-      </Column>
+        <Column>
+          <div>
+            <Text>{'<Button startIcon="anchor" />'}</Text>
+            <Button startIcon="anchor" color="primary">
+              Button
+            </Button>
+          </div>
+          <div>
+            <Text>{'<Button endIcon="rocket" />'}</Text>
+            <Button endIcon="rocket" color="danger">
+              Button
+            </Button>
+          </div>
+        </Column>
 
-      <Column>
-        <div>
-          <Text>{'<Button size="sm" />'}</Text>
-          <Button size="sm" color="primary">
-            Button
-          </Button>
-        </div>
-        <div>
-          <Text>{'<Button size="md" />'}</Text>
-          <Button size="md" color="primary">
-            Button
-          </Button>
-        </div>
-        <div>
-          <Text>{'<Button size="lg" />'}</Text>
-          <Button size="lg" color="primary">
-            Button
-          </Button>
-        </div>
-      </Column>
-      <Column>
-        <div>
-          <Text>{'<Button color="default" />'}</Text>
-          <Button color="default">Button</Button>
-        </div>
-        <div>
-          <Text>{'<Button color="primary" />'}</Text>
-          <Button color="primary">Button</Button>
-        </div>
-        <div>
-          <Text>{'<Button color="secondary" />'}</Text>
-          <Button color="secondary">Button</Button>
-        </div>
-        <div>
-          <Text>{'<Button color="danger" />'}</Text>
-          <Button color="danger">Button</Button>
-        </div>
-      </Column>
-    </Container>
+        <Column>
+          <div>
+            <Text>{'<Button size="sm" />'}</Text>
+            <Button size="sm" color="primary">
+              Button
+            </Button>
+          </div>
+          <div>
+            <Text>{'<Button size="md" />'}</Text>
+            <Button size="md" color="primary">
+              Button
+            </Button>
+          </div>
+          <div>
+            <Text>{'<Button size="lg" />'}</Text>
+            <Button size="lg" color="primary">
+              Button
+            </Button>
+          </div>
+        </Column>
+        <Column>
+          <div>
+            <Text>{'<Button color="default" />'}</Text>
+            <Button color="default">Button</Button>
+          </div>
+          <div>
+            <Text>{'<Button color="primary" />'}</Text>
+            <Button color="primary">Button</Button>
+          </div>
+          <div>
+            <Text>{'<Button color="secondary" />'}</Text>
+            <Button color="secondary">Button</Button>
+          </div>
+          <div>
+            <Text>{'<Button color="danger" />'}</Text>
+            <Button color="danger">Button</Button>
+          </div>
+        </Column>
+      </Container>
+      <Footer>
+        <SmallText>
+          Button Component done by <a href="driera">driera</a>,{" "}
+          <a
+            href="https://creativecommons.org/licenses/by-sa/4.0/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            CC-by-SA 4.0
+          </a>
+        </SmallText>
+      </Footer>
+    </>
   );
 };
 
